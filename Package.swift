be11a439fbbaba03a556197ed6739be837463b7e
xinghou.liu <x_coder_20090203@hotmail.co.uk>
2024-10-04 22:08:07 +0100

    Support from watchOS 6.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TinyRequest",
-    platforms: [.iOS("16.0"), .macOS("13.5.1"), .tvOS("13.0"), .watchOS("4.0")],
+    platforms: [.iOS("16.0"), .macOS("13.5.1"), .tvOS("13.0"), .watchOS("6.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
